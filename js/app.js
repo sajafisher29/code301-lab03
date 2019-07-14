3bed27597ff65272dab7dcdd4ae9472439107539
Sarah <47694725+sajafisher29@users.noreply.github.com>
2019-07-14 14:55:45 -0700

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,95 @@
+'use strict'
+
+//Create helper funtion to launch actions when the app is started
+
+function startApp(){
+  readFile();
+}
+
+//Constructor function to create objects with the JSON data
+
+ImageObject.list = []
+
+function ImageObject (item){
+  this.image_url = item.image_url;
+  this.title = item.title;
+  this.description = item.description;
+  this.keyword = item.keyword;
+  this.horns = item.horns;
+}
+
+//Create function to read the file, run them through the constructor function, and into the storage array
+
+function readFile(){
+
+  $.get('/data/page-1.json', 'json')
+    .then( data => {
+      data.forEach(item => {
+        ImageObject.list.push(new ImageObject(item));
+      })
+
+      displayImages();
+      ImageObject.populateFilter();
+      ImageObject.handleFilter();
+    });
+}
+
+//Create function to display images on the home page by cloning the template for each photo object
+
+function displayImages(){
+  ImageObject.list.forEach( item => {
+    const $newItem = $('.photo-template').clone();
+
+    $newItem.find('h2').text(item.title);
+    $newItem.find('img').attr('src', item.image_url).attr('alt', item.keyword);
+    $newItem.find('p').text(item.description);
+    $newItem.attr('class', item.keyword);
+    $newItem.removeClass('photo-template');
+    $('main').append($newItem);
+  });
+
+  //Remove the photo template now that we have data to add to the page
+
+  $('.photo-template').remove();
+}
+
+//Read the keyword array and fill the drop down menu with those items
+
+ImageObject.populateFilter = () => {
+  const keywordArray = [];
+
+  $('option').not(':first').remove();
+
+  ImageObject.list.forEach ( item => {
+    if(!keywordArray.includes(item.keyword)) {keywordArray.push(item.keyword);}
+  })
+
+  keywordArray.sort();
+
+  keywordArray.forEach(keyword => {
+    let $newKey = `<option value = "${keyword}">${keyword}</option>`;
+    $('select').append($newKey);
+  })
+}
+
+ImageObject.handleFilter = () => {
+  $('select').on('change', function () {
+    let $selected = $(this).value;
+    if ($selected !== 'default') {
+      $('section').hide();
+
+      ImageObject.list.forEach( item => {
+        if ($selected === item.keyword) {
+          $`section[class = "${$selected}"]`.addClass('filtered').fadeIn();
+        }
+      });
+
+      $(`option[value = ${$selected}]`).fadeIn();
+    } else {
+      $('section').removeClass('filtered').fadeIn();
+      $(`option[value = ${$selected}]`).fadeIn();
+    }
+  });
+};
+
+$(startApp);
